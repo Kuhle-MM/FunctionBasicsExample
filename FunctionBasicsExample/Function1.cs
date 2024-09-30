6e3dceb44ae32ed84a484f71fe2b5b45b9c69216
Kuhle-MM <ST10259861@vcconnect.edu.za>
2024-09-30 22:31:28 +0200

    Add project files.

---
+++ FunctionBasicsExample/Function1.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctionBasicsExample
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("Function1")]
+        public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered funtion executed successfully." +
+                " Pass a name in the query string or in the request body for a personalized response." 
+                : $"Hello, {name}. This HTTP triggered function executed successfully." ;
+
+
+            return new OkObjectResult(responseMessage);
+        }
+
+
+    }
+}
